bf0a505cfa1728191460d89fb260e69ea3d7b968
Rustie McCumber <rustie.mccumber@gmail.com>
2020-11-12 12:21:55 -0800

    updating

--- todo.rb
+++ todo.rb
@@ -1,11 +1,12 @@
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/reloader' if development?
 require 'sinatra/content_for'
 require 'tilt/erubis'
 
 configure do
   enable :sessions
   set :session_secret, 'secret'
+  set :erb, :escape_html => true
 end
 
 helpers do 
