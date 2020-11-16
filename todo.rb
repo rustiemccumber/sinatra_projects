c4fd8e19fa185e2341516b3d50d2c906c83ec00a
Rustie McCumber <rustie.mccumber@gmail.com>
2020-11-16 09:06:09 -0800

    ok

--- todo.rb
+++ todo.rb
@@ -164,7 +164,7 @@ post '/lists/:number/todos' do
     erb :specific_todo, layout: :layout
   else
     
-    id = next_todo_id(@list[:todos]) # refine this later
+    id = next_element_id(@list[:todos]) # refine this later
 
     @list[:todos] << { id: id, name: text, completed: false}
     session[:success] = "the todo was added."
